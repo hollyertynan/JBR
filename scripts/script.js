8c24a01173ddcd14f571222208a505293b15cf24
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2021-11-14 17:49:21 -0500

    Updates
    Makes buttons do different things, will use that div to make a form with those options.

--- scripts/script.js
+++ scripts/script.js
@@ -12,12 +12,40 @@ HW 1, Part 2, Author: Wenjin Zhou
 
 */
 
+// global user value
+let user = "";
+
+// when signed in, this is performed
 function onSignIn(googleUser) {
     document.getElementById("yourName").innerHTML = "Welcome, "
     document.getElementById("googleSigningForRemoval").innerHTML = "<h1>" + googleUser.getBasicProfile().getName() + "</h1>";
     document.getElementById("googleProfilePicture").innerHTML = "<h1><img src='" + googleUser.getBasicProfile().getImageUrl() + "' alt='Profile Picture'></h1>";
+
+    // set global user value
+    user = googleUser.getBasicProfile().getName();
 }
 
-// GET/SET DATE + TME
-const date = new Date();
-document.getElementById("time").innerHTML = date.toLocaleString();
+
+
+// get/set date and time
+const date = new Date().toLocaleString();
+document.getElementById("time").innerHTML = date;
+
+// function if ticket is clicked
+
+function onTicket() {
+    document.getElementById("myForm").innerHTML = "";
+    document.getElementById("myForm").innerHTML = ":)";
+}
+
+// function if phone is clicked
+function onPhone() {
+    document.getElementById("myForm").innerHTML = "";
+    document.getElementById("myForm").innerHTML = ":(";
+}
+
+// function if maintenance is clicked
+function onMaintenance() {
+    document.getElementById("myForm").innerHTML = "";
+    document.getElementById("myForm").innerHTML = ":O";
+}
