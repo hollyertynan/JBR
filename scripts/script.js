5bf74d23c5a1e0f341509c21738b60f0b46f1761
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2021-11-14 14:01:30 -0500

    Updates
    Added Google Integration

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,17 @@ HW 1, Part 2, Author: Wenjin Zhou
 
 */
 
-const date = new Date();
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 
+
+// GET/SET DATE + TME
+const date = new Date();
 document.getElementById("time").innerHTML = date.toLocaleString();
+
+document.getElementById("yourName").innerHTML = profile.getName();
