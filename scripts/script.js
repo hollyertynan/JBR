05989ac7dddfbb2245f0e6740a69bf2acb251b81
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2021-11-14 10:09:03 -0500

    Updates
    Included correct documentation regarding authors, will be updated further as we progress.

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/*
+    
+File: style.js
+Authors: Tynan Hollyer, Damian Oakes
+tynan_hollyer@student.uml.edu
+
+Description: JBR JS file. 
+
+Credit:
+Proper Documentation copied from
+HW 1, Part 2, Author: Wenjin Zhou
+
+*/
