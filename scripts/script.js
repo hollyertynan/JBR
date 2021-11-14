2e236b83a4139236cc3e7af45f90a48689d4373e
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2021-11-14 14:35:00 -0500

    Updates
    More Google Updates

--- scripts/script.js
+++ scripts/script.js
@@ -13,14 +13,6 @@ HW 1, Part 2, Author: Wenjin Zhou
 */
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-function onSuccess(googleUser) {
     document.getElementById("yourName").innerHTML = "Welcome, "
     document.getElementById("googleSigningForRemoval").innerHTML = googleUser.getBasicProfile().getName();
     console.log(document.getElementById("googleSigningForRemoval").innerHTML = googleUser.getBasicProfile().getName());
