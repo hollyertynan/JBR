e44782d45391805abdbb18926a8b28e5d5eded0a
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-01-03 12:19:24 -0500

    updates to fix my commit error

--- scripts/script.js
+++ scripts/script.js
@@ -267,7 +267,7 @@ function onEvent(event) {
 function onRegister() {
     document.getElementById("thirdForm").innerHTML = "";
     document.getElementById("fourthForm").innerHTML = "";
-    document.getElementById("thirdForm").innerHTML += "<div class=\"container-fluid bg-dark text-center pb-5\"><div class=\"row\"><div class=\"col-3\"></div><div class=\"col-3\"><button id=\"hardware\" type=\"button\" class=\"btn btn-info text-light InitialButtons\" onclick=\"onHardware()\"><h4>Hardware</h4></button></div><div class=\"col-3\"><button id=\"software\" type=\"button\" class=\"btn btn-info text-light InitialButtons\" onclick=\"onSoftware()\"><h4 >Software</h4></button></div><div class=\"col-3\"></div></div></div>";
+    document.getElementById("thirdForm").innerHTML += "<div class=\"container-fluid bg-dark text-center pb-5\"><div class=\"row\"><div class=\"col-2\"></div><div class=\"col-4\"><button id=\"hardware\" type=\"button\" class=\"btn btn-info text-light InitialButtons\" onclick=\"onHardware()\"><h4>Hardware</h4></button></div><div class=\"col-4\"><button id=\"software\" type=\"button\" class=\"btn btn-info text-light InitialButtons\" onclick=\"onSoftware()\"><h4 >Software</h4></button></div><div class=\"col-2\"></div></div></div>";
 
     document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
 }
