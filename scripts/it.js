23ea8b2779bd797ca9e1d65ebf85b6476f501876
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-01-04 08:09:27 -0500

    added all items on IT dropdown w/o continuing informatoin

--- scripts/it.js
+++ scripts/it.js
@@ -19,6 +19,48 @@ function onEventIT(event) {
         case "4":
             onEmailServer();
             break;
+        case "5":
+            onPrinters();
+            break;
+        case "6":
+            onKiosk();
+            break;
+        case "7":
+            onChromebook();
+            break; 
+        case "8":
+            onIpad();
+            break; 
+        case "9":
+            onPaintComputer();
+            break; 
+        case "10":
+            onReports();
+            break; 
+        case "11":
+            onOnlineOrder();
+            break; 
+        case "12":
+            onItMaintenance();
+            break; 
+        case "13":
+            onRequestNewPhone();
+            break; 
+        case "14":
+            onVOIP();
+            break; 
+        case "15":
+            onSecurity();
+            break; 
+        case "16":
+            onWifi();
+            break; 
+        case "17":
+            onV9();
+            break; 
+        case "18":
+            onITOther();
+            break; 
         default: 
             blank();
             break;
@@ -99,6 +141,202 @@ onEmailServer() pipeline
 */
 
 function onEmailServer() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onPrinters() pipeline
+
+*/
+
+function onPrinters() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onKiosk() pipeline
+
+*/
+
+function onKiosk() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onChromebook() pipeline
+
+*/
+
+function onChromebook() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onIpad() pipeline
+
+*/
+
+function onIpad() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onPaintComputer() pipeline
+
+*/
+
+function onPaintComputer() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onReports() pipeline
+
+*/
+
+function onReports() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onOnlineOrder() pipeline
+
+*/
+
+function onOnlineOrder() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onItMaintenance() pipeline
+
+*/
+
+function onItMaintenance() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onRequestNewPhone() pipeline
+
+*/
+
+function onRequestNewPhone() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onVOIP() pipeline
+
+*/
+
+function onVOIP() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onSecurity() pipeline
+
+*/
+
+function onSecurity() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onWifi() pipeline
+
+*/
+
+function onWifi() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onV9() pipeline
+
+*/
+
+function onV9() {
+    document.getElementById("thirdForm").innerHTML = "";
+    document.getElementById("fourthForm").innerHTML = "";
+    document.getElementById("thirdForm").innerHTML += ":)";
+
+    document.getElementById("thirdForm").scrollIntoView({behavior: "smooth"});
+}
+
+/*
+
+onITOther() pipeline
+
+*/
+
+function onITOther() {
     document.getElementById("thirdForm").innerHTML = "";
     document.getElementById("fourthForm").innerHTML = "";
     document.getElementById("thirdForm").innerHTML += ":)";
